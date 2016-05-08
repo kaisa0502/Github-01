cb99627302b70f2a3c2dbe523b9abbeec5f8d0fc
kaisa.huang <kaisa0502@163.com>
2016-05-08 22:56:46 +0800

    初始化项目.

---
+++ TEST-01-FOR-GITHUB/TEST-01-FOR-GITHUB/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TEST-01-FOR-GITHUB
+//
+//  Created by kaisa.huang on 16/5/8.
+//  Copyright © 2016年 kaisa.huang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TEST-01-FOR-GITHUB/TEST-01-FOR-GITHUB/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TEST-01-FOR-GITHUB
+//
+//  Created by kaisa.huang on 16/5/8.
+//  Copyright © 2016年 kaisa.huang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
