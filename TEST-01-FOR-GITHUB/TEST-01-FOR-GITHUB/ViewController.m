7bf50c16fdee2aadbfadb72237bb195782d4b4c2
kaisa.huang <kaisa0502@163.com>
2016-05-08 23:33:41 +0800

    123.

--- TEST-01-FOR-GITHUB/TEST-01-FOR-GITHUB/ViewController.m
+++ TEST-01-FOR-GITHUB/TEST-01-FOR-GITHUB/ViewController.m
@@ -19,6 +19,7 @@
     // Do any additional setup after loading the view, typically from a nib.
     NSLog(@"THIS IS MY FIRST COMMIT");
     NSLog(@"abc");
+    NSLog(@"123");
 }
 
 - (void)didReceiveMemoryWarning {
